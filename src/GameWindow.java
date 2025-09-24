c1037e70e591cfe5f3cafca46f95ef4d9e09e59f
dtucker <davidt2003@msn.com>
2025-09-24 16:27:28 -0400

    feat: implement GameWindow

---
+++ src/GameWindow.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GameWindow extends JFrame {
+
+    private JPanel panel;
+
+    public GameWindow() {
+        setSize(800, 600);
+        setLocationRelativeTo(null); // Center frame
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setState(JFrame.NORMAL);
+
+        panel = new JPanel();
+        panel.setBackground(Color.BLUE);
+        panel.setFocusable(true);
+        panel.setDoubleBuffered(true);
+        add(panel);
+    }
+
+    public void start() {
+        setVisible(true);
+    }
+}
