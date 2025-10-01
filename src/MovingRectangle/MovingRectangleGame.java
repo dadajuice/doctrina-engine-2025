2650a633ff49f2d4d9b5c4a57a4897bae314dc69
dtucker <davidt2003@msn.com>
2025-10-01 17:36:21 -0400

    feat: amazing new feature

--- src/MovingRectangle/MovingRectangleGame.java
+++ src/MovingRectangle/MovingRectangleGame.java
@@ -3,20 +3,29 @@ package MovingRectangle;
 import Doctrina.Canvas;
 import Doctrina.Game;
 
+import java.awt.*;
+
 public class MovingRectangleGame extends Game {
+
+    private Player player;
+    private Npc npc;
 
     @Override
     public void initialize() {
-
+        player = new Player();
+        npc = new Npc();
     }
 
     @Override
     public void update() {
-
+        player.update();
+        npc.update();
     }
 
     @Override
     public void draw(Canvas canvas) {
-
+        canvas.drawRectangle(0, 0, 800, 600, Color.BLUE);
+        player.draw(canvas);
+        npc.draw(canvas);
     }
 }
