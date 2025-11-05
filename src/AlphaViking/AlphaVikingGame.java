a34cdf3cc5f939c9f6bcb0152097700e3882421c
dtucker <davidt2003@msn.com>
2025-11-05 16:37:40 -0500

    feat: implement music

--- src/AlphaViking/AlphaVikingGame.java
+++ src/AlphaViking/AlphaVikingGame.java
@@ -23,6 +23,18 @@ public class AlphaVikingGame extends Game {
         world = new World();
         world.load();
         tree = new Tree(300, 350);
+
+        try {
+            Clip clip = AudioSystem.getClip();
+            AudioInputStream stream = AudioSystem.getAudioInputStream(
+                this.getClass().getClassLoader().getResourceAsStream("audios/music.wav")
+            );
+            clip.open(stream);
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+            clip.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
